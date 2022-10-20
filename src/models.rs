a06f2cd3277813b62fec1a5867636fd9264e92fd
Grapphy <76534455+Grapphy@users.noreply.github.com>
2022-10-20 20:25:30 -0300

    Adding models for JSON responses

--- src/models.rs
+++ src/models.rs
@@ -11,6 +11,28 @@ pub struct Certificate {
     pub serial_number: String,
 }
 
+#[derive(Debug, Deserialize)]
+pub struct DNSEntry {
+    pub address: Option<String>,
+    pub hostname: Option<String>,
+    pub record_type: Option<String>,
+    pub asset_type: Option<String>,
+    pub asn: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AlientVaultDNS {
+    pub passive_dns: Vec<DNSEntry>,
+    pub count: u64,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ThreatminerResults {
+    pub status_code: String,
+    pub status_message: String,
+    pub results: Vec<String>,
+}
+
 #[derive(Debug)]
 pub struct Subdomain {
     pub url: String,
